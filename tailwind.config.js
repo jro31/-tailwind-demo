5d9d907b26962f18d65d7386935f95b3dcace38a
Jethro Williams <jethrowilliams@outlook.com>
2021-11-25 18:06:05 +0000

    Set-up tailwind locally
    Add 'package.json' by running 'npm init -y'.
    
    Install 'tailwind', 'postcss', 'autoprefixer' and 'vite' with 'npm install -D tailwindcss postcss autoprefixer vite' (and update "scripts" in package.json to '"dev": "vite"').
    
    Run 'npx tailwindcss init -p' to add the 'tailwind.config.js' and 'postcss.config.js' files.
    
    Updated 'index.html' to use '/css/tailwind/css' instead of the CDN.
    
    (also delete the 'build' folder from the last commit)

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// This is the place that you come when you want to customise tailwind
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
