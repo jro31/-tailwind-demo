7998c897277c6f7f946684869cd58134f9254600
Jethro Williams <jethrowilliams@outlook.com>
2021-11-26 13:57:19 +0000

    Give an example of chaining breakpoints (sm) with state (hover) 'sm:hover:bg-green-500', and update the 'tailwind.config.js' file to allow 'active' state styling ('active:bg-indigo-600')

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-// This is the place that you come when you want to customise tailwind
-
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
@@ -7,7 +5,13 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ['active'],
+    },
   },
   plugins: [],
 };
+
+// To enable 'active' to work for the background color, we must set it here
+// See 'https://tailwindcss.com/docs/configuring-variants' for a list of variants (for example, 'active'), that we can set
+// See 'https://tailwindcss.com/docs/configuration#variant-order' for a list of core plugins (for example, 'backgroundColor')
