a19cbd6ef117c0a158361d09c27a84f934c32b58
Jethro Williams <jethrowilliams@outlook.com>
2021-11-26 16:15:29 +0000

    Create a 'btn' class that can be used globally, that uses all the styling declarations from our existing button
    And an explanation of why we're wrapping the '.btn' class declaration in '@layer components'

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,3 @@ module.exports = {
   },
   plugins: [],
 };
-
-// State variants (hover, focus, active etc) are only enabled by default for the most common use cases (such as changing a background colour when hovering)
-// For less common use cases (such as changing the font-size on hover), you need to enable them here
